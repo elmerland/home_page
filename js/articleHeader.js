2065145f4bd8ee78419bee05512625ff244438c2
Elmer Landaverde <elmer@vt.edu>
2014-01-08 10:09:12 -0600

    Added some images

--- js/articleHeader.js
+++ js/articleHeader.js
@@ -1,6 +1,29 @@
 window.onload = function () {
 	addPageNav();
 	addPageHeader();
+	backToTopAnimation();
+}
+
+function backToTopAnimation() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 250) {
+		    $('.toTop').fadeIn();
+		} else {
+		    $('.toTop').fadeOut();
+		}
+	});
+	
+	$('.toTop').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});
+	
+	document.onclick = function (e) {
+		e = e ||  window.event;
+		var element = e.target || e.srcElement;
+		if (element.tagName == 'A' && element.className == "toTop") {
+			return false; // prevent default action and stop event propagation
+		}
+	};
 }
 
 function addPageHeader() {
