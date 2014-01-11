5707f164b7c60ae467a636e6784d7620955a5fab
Elmer Landaverde <elmer@vt.edu>
2014-01-11 11:36:43 -0600

    more stuff

--- pages/about.php
+++ pages/about.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="../css/article.css" />
 	<link rel="stylesheet" type="text/css" href="../css/code.css" />
 	<script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-	<script language="javascript" type="text/javascript" src="../js/articleHeader.js"></script>
+	<script language="javascript" type="text/javascript" src="../js/article.js"></script>
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -21,7 +21,11 @@
 
 	</script>
 </head>
-<body id="start">
+<body>
+	<?php
+		include("../php/header.php"); 
+		include("../php/nav.php");
+	?>
 	<article class="main">
 		
 		<header>
@@ -40,14 +44,14 @@
 		<div id="content">
 			
 			<h2 id="whoAmI">Who Am I?</h2>
-			<span><a class="toTop" href="#start">Back to top</a></span>
+			<span><a class="toTop" href="#">Back to top</a></span>
 		
 			<p>
 				My Name is Elmer Landaverde. Currently I am a senior at Virginia Tech, doing a double major in computer science and business management. But that’s the boring part. More to the point I am a guy who is passionate about learning. My interests vary from sociology, math, statistics to design, photography and aesthetics. Mostly however I just enjoy learning about different things to try and figure out this darn world we live in. Out of all my interests the one I focus on the most and have had the longest is in Computer Science. This passion started when I took my first Java class in high school and has being with me ever since. I hope some of what I write in this webpage is helpful to somebody but most of all this webpage is helpful to me because it serves as visual map of the projects that I embark on.
 			</p>
 	
 			<h2 id="purpose">Purpose Of This Website</h2>
-			<span><a class="toTop" href="#start">Back to top</a></span>
+			<span><a class="toTop" href="#">Back to top</a></span>
 	
 			<p>
 				The idea for this website has being long in the making. However it wasn’t until recently that I had to learn JavaScript for a research project I’m about to be part of, and because the most important part about learning is doing I figured it would be a good practice to finally create a web page to get some practice. This is how I came about to create this site. Initially I thought I would just make a <a title="Word Press" href="http://wordpress.org/" target="_blank">WordPress</a> blog, but that ended quickly. Partly because I didn’t find a theme I liked, and mostly because typing it up your-self is more fun :)
