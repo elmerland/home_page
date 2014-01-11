131dd622d1b1f63ff7b938269ace0f04130e6bb0
Elmer Landaverde <elmer@vt.edu>
2014-01-11 17:36:28 -0600

    Changed link structure

--- articles/dynamic-table.php
+++ articles/dynamic-table.php
@@ -1,26 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+	<?php
+		require_once("../php/articleLinks.php");
+	?>
 	<title>Creating a Dynamically Sized Table with JavaScript</title>
-	<link rel="icon" type="image/png" href="../_images/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="../css/normalize.css" />
-	<link rel="stylesheet" type="text/css" href="../css/header.css" />
-	<link rel="stylesheet" type="text/css" href="../css/nav.css" />
-	<link rel="stylesheet" type="text/css" href="../css/article.css" />
-	<link rel="stylesheet" type="text/css" href="../css/code.css" />
-	<script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-	<script language="javascript" type="text/javascript" src="../js/article.js"></script>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-46960971-1', 'learncodeart.com');
-	  ga('send', 'pageview');
-
-	</script>
 </head>
 <body>
 	<?php
