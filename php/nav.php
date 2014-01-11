466985693ca4d19b55a1d12b01f6a3650aef8330
Elmer Landaverde <elmer@vt.edu>
2014-01-11 17:26:08 -0600

    Changes css styling

--- php/nav.php
+++ php/nav.php
@@ -1,13 +1,13 @@
 <nav id="pageNav">
 	<ul>
 		<li>
-			<a title="Home" href="http://learncodeart.com/pages/index.html">Home</a>
+			<a title="Home" href="http://learncodeart.com/index.php">Home</a>
 		</li>
 		<li>
-			<a title="About" href="http://learncodeart.com/pages/about.html">About</a>
+			<a title="About" href="http://learncodeart.com/pages/about.php">About</a>
 		</li>
 		<li>
-			<a title="Resume" href="http://learncodeart.com/pages/resume.html">Résumé</a>
+			<a title="Resume" href="http://learncodeart.com/pages/resume.php">Résumé</a>
 		</li>
 	</ul>
 </nav>
