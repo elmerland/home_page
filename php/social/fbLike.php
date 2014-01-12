79b43c73663ebacde0c86efa57513a091947257f
Elmer Landaverde <elmer@vt.edu>
2014-01-12 10:52:32 -0600

    PHP facebook like

--- php/social/fbLike.php
+++ php/social/fbLike.php
@@ -1,5 +1,5 @@
 <?php
-$url="\"http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\"";
+$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 echo $url;
 ?>
-<div class="fb-like" data-href=$url data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+<div class="fb-like" data-href="$url" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
