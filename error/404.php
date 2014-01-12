369b59e62fd8416100cc1f82ed341671fd79b1c1
Elmer Landaverde <elmer@vt.edu>
2014-01-11 19:15:37 -0600

    Fixed 404 page

--- error/404.php
+++ error/404.php
@@ -22,7 +22,7 @@
 						</a>
 					</h3>
 				</header>
-				<iframe width="560" height="315" src="http://www.youtube.com/embed/oqMl5CRoFdk" frameborder="0" allowfullscreen></iframe>
+				<iframe width="590" height="332" src="http://www.youtube.com/embed/oqMl5CRoFdk?start=5&amp;feature=oembed&amp;loop=1&amp;autoplay=1" frameborder="0" allowfullscreen=""></iframe>
 				<header>
 					<h3>
 						<a title="404 Error" href="#">
@@ -32,7 +32,7 @@
 				</header>
 				<p>
 					But if you try sometimes,<br>
-					You just might find what you need.
+					You just might find you get what you need.
 					<a title="Rolling stiones 404" href="http://www.rollingstones.com/404" target="_blank">Rollingstones 404 page</a>
 				</p>
 				<div class="clear"></div>
