7775d8d68f8049e56631cc6dc65ac32cfa67fed1
Elmer Landaverde <elmer@vt.edu>
2014-01-21 19:37:01 -0500

    Changed prettypring theme

--- php/articleCSS_JS.php
+++ php/articleCSS_JS.php
@@ -3,4 +3,4 @@
 
 <script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 <script language="javascript" type="text/javascript" src="../js/article.js"></script>
-<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=sons-of-obsidian"></script>
+<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
