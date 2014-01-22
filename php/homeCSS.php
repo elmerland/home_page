8f7a0043013f50718a587c3c627f889ed5961c96
Elmer Landaverde <elmer@vt.edu>
2014-01-21 20:30:29 -0500

    Added styling for pre tags

--- php/homeCSS.php
+++ php/homeCSS.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/main.css" />
-<link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/article.css" />
+<link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/article_excerpt.css" />
