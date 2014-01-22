f8258bf8ecf74ec62a0705a8c85943359c6f012d
Elmer Landaverde <elmer@vt.edu>
2014-01-21 20:06:55 -0500

    Fix path bug

--- php/articleCSS_JS.php
+++ php/articleCSS_JS.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/article.css" />
 <link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/code.css" />
-<link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/jquery.snippet.css" />
+<link rel="stylesheet" type="text/css" href="http://learncodeart.com/css/jquery.snippet.min.css" />
 
 <script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 <script language="javascript" type="text/javascript" src="http://learncodeart.com/js/article.js"></script>
 
 
-<script type="text/javascript" src="http://learncodeart.com/js/jquery.snippet.js"></script>
+<script type="text/javascript" src="http://learncodeart.com/js/jquery.snippet.min.js"></script>
 
 <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
