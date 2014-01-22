fc73d7f9b10fd01e0315d95c6dd282e1137eadb6
Elmer Landaverde <elmer@vt.edu>
2014-01-21 20:02:55 -0500

    bug

--- php/articleCSS_JS.php
+++ php/articleCSS_JS.php
@@ -4,6 +4,8 @@
 
 <script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 <script language="javascript" type="text/javascript" src="http://learncodeart.com/js/article.js"></script>
-<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+
 
 <script type="text/javascript" src="http://learncodeart.com/js/jquery.snippet.js"></script>
+
+<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
