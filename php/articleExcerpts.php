9159bfcf8e795df40267a24748c9473fb3d80079
Elmer Landaverde <elmer@vt.edu>
2014-01-30 02:17:40 -0500

    Added two excerpts for two new articles

--- php/articleExcerpts.php
+++ php/articleExcerpts.php
@@ -1,13 +1,16 @@
 <article class="excerpt">
 	<header>
-		<h3>
-			<a title="Creating a Dynamically Sized Table with JavaScript" href="http://learncodeart.com/articles/dynamic_table.php" rel="alternate">
-				Creating a Dynamically Sized Table with JavaScript
-			</a>
-		</h3>
+		<a title="OpenDSA: Getting Acquainted" href="#" rel="alternate">
+			<h3>
+				OpenDSA: Getting Started With JSAV
+			</h3>
+		</a>
 	</header>
+	<h4>
+		Coming soon!
+	</h4>
 	<p>
-		In this article we will go step by step to create a dynamically sized table with JavaScript. We will use some simple HTML and CSS to give the table structure and styling and then we will proceed to create our JavaScript code that will fetch information from the user and create a table of variable size.
+		The previous articles went through a high level overview of the OpenDSA project. This article starts going deeper into the details of the project and will give you enough information for you to get started with JSAV. It will teach you everything you need to know to create your first diagram using JSAV and more importantly it will go through some good practices that will make your code easier to maintain and reduce any conflicts with other developers’ code.
 	</p>
 	<footer>
 		<p>
@@ -19,11 +22,52 @@
 </article>
 <article class="excerpt">
 	<header>
-		<h3>
-			<a title="Creating a little snake game" href="http://learncodeart.com/articles/snake_game.php" rel="alternate">
+		<a title="OpenDSA: Getting Acquainted" href="#" rel="alternate">
+			<h3>
+				OpenDSA: Getting Acquainted
+			</h3>
+		</a>
+	</header>
+	<h4>
+		Coming soon!
+	</h4>
+	<p>
+		Starting on a new project can be a daunting task, especially if the project has a lot of files and complex organization. Over time I have found that getting a high level overview of how the project works has helped me understand how all the details fit together. This is why in this article I will explain all you need to know to get acquainted with the OpenDSA project. At the end of the article you should be familiar with how the project is set up and understand all the parts you have to consider to start creating new content using JSAV.
+	</p>
+	<footer>
+		<p>
+			Written by: Elmer Landaverde<br>
+			January 9, 2014
+		</p>
+	</footer>
+	<div class="clear"></div>
+</article>
+<article class="excerpt">
+	<header>
+		<a title="Creating a Dynamically Sized Table with JavaScript" href="http://learncodeart.com/articles/dynamic_table.php" rel="alternate">
+			<h3>
+				Creating a Dynamically Sized Table with JavaScript
+			</h3>
+		</a>
+	</header>
+	<p>
+		In this article we will go step by step to create a dynamically sized table with JavaScript. We will use some simple HTML and CSS to give the table structure and styling and then we will proceed to create our JavaScript code that will fetch information from the user and create a table of variable size.
+	</p>
+	<footer>
+		<p>
+			Written by: Elmer Landaverde<br>
+			January 30, 2014
+		</p>
+	</footer>
+	<div class="clear"></div>
+</article>
+<article class="excerpt">
+	<header>
+		<a title="Creating a little snake game" href="http://learncodeart.com/articles/snake_game.php" rel="alternate">
+			<h3>
 				Creating a Little Snake Game
-			</a>
-		</h3>
+			</h3>
+		</a>
 	</header>
 	<p>
 		The creation of a web browser Snake game. This is a basic Snake game created using HTML, CSS, and JavaScript. The main focus of this project is on the JavaScript part so the HTML and CSS files are fairly small. I go through all the steps of how I came to start this project and the details of how the JavaScript makes it work.
