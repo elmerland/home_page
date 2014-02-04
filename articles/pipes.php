010e3158a3b1f41913f7861dbfa1780ca01226ab
Elmer Landaverde <elmer@vt.edu>
2014-02-04 14:06:45 -0500

    Created new article

---
+++ articles/pipes.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		require_once("../php/mainCSS.php");
+		require_once("../php/articleCSS_JS.php");
+	?>
+	<title>Pipes and Processes</title>
+</head>
+<body>
+	<?php
+		require_once("../php/social/fbJS.php");
+		require_once("../php/header.php");
+		require_once("../php/nav.php");
+	?>
+
+	<div class="banner" style="background-image: url(http://learncodeart.com/_images/patterns/asteroids.jpg);"></div>
+
+	<article class="main">
+		
+		<nav class="outline float">
+			<section>
+				<h2>Outline</h2>
+				<ul>
+					<a href="#intro"><li>Intro</li></a>
+					<a href="#fd"><li>File Descriptors</li></a>
+					<a href="#pipes"><li>Pipes</li></a>
+					<a href="#close"><li>Close command</li></a>
+					<a href="#dup"><li>Dup Command</li></a>
+					<a href="#notes"><li>Notes</li></a>
+					<a href="#disqus_thread"><li>Comments</li></a>
+				</ul>
+			</section>
+		</nav>
+
+		<div class="content">
+
+			<header>
+				<h1>CS: Pipes and Processes</h1>
+				<p><time pubdate datetime="2014-00-00">Created on: Month 00, 2014</time></p>
+				<section class="social">
+					<?php
+						include("../php/social/fbLike.php");
+						include("../php/social/twitter.php");
+					?>
+				</section>
+			</header>
+			
+			<section id="intro">
+				<header>
+					<h2>Intro</h2>
+					<a href="#intro"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+			
+				<p>
+				</p>
+			</section>
+	
+			<section id="fd">
+				<header>
+					<h2>File Descriptors</h2>
+					<a href="#fd"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+	
+				<p>
+				</p>
+			</section>
+		
+			<section id="pipes">
+				<header>
+					<h2>Pipes</h2>
+					<a href="#pipes"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+		
+				<p>
+				</p>
+			</section>
+	
+			<section id="close">
+				<header>
+					<h2>Close Command</h2>
+					<a href="#close"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+	
+				<section id="step1">
+					<header>
+						<h3>Step 1</h3>
+					</header>
+			
+					<p>
+					</p>
+				</section>
+		
+				<section id="step2">
+					<header>
+						<h3>Step 2</h3>
+					</header>
+			
+					<p>
+					</p>
+				</section>
+			</section>
+	
+			<section id="concduplusion">
+				<header>
+					<h2>Dup Command</h2>
+					<a href="#dup"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+		
+				<p>
+				</p>
+			</section>
+	
+			<section id="notes">
+				<header>
+					<h2>Notes</h2>
+					<a href="#notes"><img src="http://learncodeart.com/_images/link20.png"></a>
+					<a class="toTop" href="#"><img src="http://learncodeart.com/_images/toTop.png"></a>
+				</header>
+		
+				<p>
+				</p>
+			</section>
+			
+			<section class="social">
+				<?php
+					include("../php/social/fbLike.php");
+					include("../php/social/twitter.php");
+				?>
+			</section>
+			
+			<footer>
+				<p>
+					Written by: Elmer Landaverde <a href="mailto:elmerlandaverde@gmail.com">(elmerlandaverde@gmail.com)</a><br>
+					Last updated: Month 00, 2014<br>
+					<a title="Nerdfighters" href="http://nerdfighters.ning.com/" target="_blank">DFTBA!</a>
+				</p>
+			</footer>
+			<?php require_once("../php/disqus.php"); ?>
+		
+		</div>
+	</article>
+</body>
+</html>
